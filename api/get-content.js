ee2ab89077f2fce7c1bf6d37ef214500f010bd50
Sue Hovhan <87306378+SyuneHovan@users.noreply.github.com>
2025-03-27 16:25:35 +0400

    t

--- api/get-content.js
+++ api/get-content.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     const GITHUB_REPO = "SyuneHovan/syuniq";
-    const { file } = req.query; // Get FILE_PATH from query parameter
+    // const { file } = req.query; // Get FILE_PATH from query parameter
     const file = "content.html"; // Get FILE_PATH from query parameter
     const GITHUB_TOKEN = process.env.GITHUB_TOKEN; 
 
