aea1a86dd0a83b056fd69d483429a816a2e556df
Sue Hovhan <87306378+SyuneHovan@users.noreply.github.com>
2025-04-02 13:53:06 +0400

    s

--- public/script.js
+++ public/script.js
@@ -45,6 +45,14 @@ function applyItalic() {
     document.execCommand('italic');
 }
 
+function applyBlue() {
+    document.execCommand('foreColor', false, '#7bdbdd');
+}
+
+function applyRed() {
+    document.execCommand('foreColor', false, '#dd7e6b');
+}
+
 function applyNote() {
     // Ensure the selection exists and the browser supports execCommand
     var selection = window.getSelection();
